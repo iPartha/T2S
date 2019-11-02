7fb1fffb7fab29c16bea20c75adaa1048c911ea7
Parthasarathi Kathiresan <sasisarathi@gmail.com>
2019-11-02 18:07:21 +0530

    Changes for create the recycler view adapter and draw the contents

--- app/src/main/java/com/ipartha/t2s/MainActivity.kt
+++ app/src/main/java/com/ipartha/t2s/MainActivity.kt
@@ -1,11 +1,8 @@
 
 package com.ipartha.t2s
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -21,27 +18,25 @@ import com.ipartha.t2s.networking.RetrofitService
 import com.ipartha.t2s.roomdb.DBConstants
 import com.ipartha.t2s.roomdb.T2SRoomDB
 import com.ipartha.t2s.ui.ConsumerMenuAdapter
+import com.ipartha.t2s.ui.VerticalItemDecoration
 import com.ipartha.t2s.ui.hide
 import com.ipartha.t2s.ui.show
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import com.ipartha.t2s.ui.MyRecyclerViewAdapter
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var consumerMenuViewModel : ConsumerMenuViewModel
-    lateinit var mBinding  : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding  = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        initView()
+        val binding  = DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
+        initView(binding)
     }
 
-    private fun initView() {
+    private fun initView(binding : ActivityMainBinding) {
+
+        binding.recyclerView.addItemDecoration(
+            VerticalItemDecoration(resources.getDimension(R.dimen.margin).toInt(), true) )
 
         val localDB =
             Room.databaseBuilder(applicationContext, T2SRoomDB::class.java, DBConstants.DB_NAME)
@@ -54,24 +49,24 @@ class MainActivity : AppCompatActivity() {
             ConsumerMenuViewModelFactory(consumerMenuRepository)
         ).get(ConsumerMenuViewModel::class.java)
 
-        observeConsumerMenu()
+        observeConsumerMenu(binding)
     }
 
-    private fun observeConsumerMenu() {
+    private fun observeConsumerMenu(binding : ActivityMainBinding) {
         consumerMenuViewModel.consumerMenu.observe(this, Observer { result ->
             when (result.status) {
                 Result.Status.SUCCESS -> {
-                    mBinding.progressBar.hide()
+                    binding.progressBar.hide()
                     result.data?.let {
-                        val adapter = MyRecyclerViewAdapter(it)
-                        mBinding.recyclerView.adapter = adapter
+                        val adapter = ConsumerMenuAdapter(it)
+                        binding.recyclerView.adapter = adapter
                     }
                 }
                 Result.Status.LOADING -> {
-                    mBinding.progressBar.show()
+                    binding.progressBar.show()
                 }
                 Result.Status.ERROR -> {
-                    mBinding.progressBar.hide()
+                    binding.progressBar.hide()
                     //Snackbar.make(binding.root, result.message!!, Snackbar.LENGTH_LONG).show()
                 }
             }
--- app/src/main/java/com/ipartha/t2s/ui/ConsumerMenuAdapter.kt
+++ app/src/main/java/com/ipartha/t2s/ui/ConsumerMenuAdapter.kt
@@ -1,61 +1,47 @@
 package com.ipartha.t2s.ui
 
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.ipartha.t2s.BR
-import com.ipartha.t2s.databinding.ActivityMainBinding
+import com.ipartha.t2s.R
+import com.ipartha.t2s.databinding.ConsumerMenuListBinding
 import com.ipartha.t2s.mvvm.ConsumerMenu
 
-class ConsumerMenuAdapter() : ListAdapter<ConsumerMenu, ConsumerMenuAdapter.ViewHolder>(DiffCallback()) {
+
+class ConsumerMenuAdapter(private val dataModelList: List<ConsumerMenu>) :
+    RecyclerView.Adapter<ConsumerMenuAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val binding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.consumer_menu_list, parent, false
+        ) as ConsumerMenuListBinding
+
+        return ViewHolder(binding)
+    }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val consumerMenu = getItem(position)
-        holder.apply {
-            bind(consumerMenu)
-            itemView.tag = consumerMenu
-        }
+        val dataModel = dataModelList[position]
+        holder.bind(dataModel)
     }
+
 
     override fun getItemCount(): Int {
-        return currentList.size
+        return dataModelList.size
     }
 
-    override fun getItem(position: Int): ConsumerMenu {
-        return currentList[position]
-    }
+    inner class ViewHolder(var binding: ConsumerMenuListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(ActivityMainBinding.inflate(
-            LayoutInflater.from(parent.context), parent, false))
-    }
-
-    class ViewHolder(private val binding : ActivityMainBinding) : RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(item : ConsumerMenu) {
-            //binding.setVariable(BR.consumerMenu, item)
-            binding.apply {
-                binding.setVariable(BR.consumerMenu, item)
-                executePendingBindings()
-            }
+        fun bind(obj: Any) {
+            binding.setVariable(BR.consumerMenu, obj)
+            binding.executePendingBindings()
         }
-
-    }
-
-    private class DiffCallback : DiffUtil.ItemCallback<ConsumerMenu>() {
-        override fun areContentsTheSame(oldItem: ConsumerMenu, newItem: ConsumerMenu): Boolean {
-            return oldItem.row == newItem.row
-        }
-
-        override fun areItemsTheSame(oldItem: ConsumerMenu, newItem: ConsumerMenu): Boolean {
-            return oldItem == newItem
-        }
-
-
     }
 
 }
