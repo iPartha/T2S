a6069e4475b64a953ab6eba6ffcb3db395de554f
Parthasarathi Kathiresan <sasisarathi@gmail.com>
2019-11-02 19:35:38 +0530

    Changes for read from DB

--- app/src/main/java/com/ipartha/t2s/data/ResultLiveData.kt
+++ app/src/main/java/com/ipartha/t2s/data/ResultLiveData.kt
@@ -2,17 +2,13 @@ package com.ipartha.t2s.data
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
-import androidx.lifecycle.map
 import kotlinx.coroutines.Dispatchers
 
-fun <T, A> resultLiveData(databaseQuery: () -> T,
+fun <T, A> resultLiveData(databaseQuery: suspend () -> T,
                           networkCall: suspend () -> Result<A>,
                           saveCallResult: suspend (A) -> Unit): LiveData<Result<T>> =
     liveData(Dispatchers.IO) {
         emit(Result.loading<T>())
-//        val source = databaseQuery.invoke().map {
-//           Result.success(it)
-//        }
         val data = databaseQuery()
         if (data == null || (data is List<*> && data.size == 0)) {
             val responseStatus = networkCall.invoke()
